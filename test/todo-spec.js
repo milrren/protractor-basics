e5b6051a1bc06e14d62f03e7c1254aab48105a2d
Milrren Mattar <milrren.mattar@minutrade.com>
2016-10-04 12:25:14 -0300

    Including another test and creating the beforeEach

--- test/todo-spec.js
+++ test/todo-spec.js
@@ -2,15 +2,27 @@ var main = require('./page-objects/main.js');
 
 describe('Protractor demo app', function() {
 
-  it('should add one and two', function() {
-
+  beforeEach(function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
+  });
+
+  it('should add one and two', function() {
 
     main.text.first.sendKeys('1');
     main.text.second.sendKeys('2');
     main.button.go.click();
 
     expect(main.label.latest.getText()).toEqual('3');
+
+  });
+
+  it('should add three and seven', function() {
+
+    main.text.first.sendKeys('3');
+    main.text.second.sendKeys('7');
+    main.button.go.click();
+
+    expect(main.label.latest.getText()).toEqual('10');
 
   });
 
