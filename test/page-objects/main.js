4a925ff5ad351d7a8014b75ccd24160905b0c203
Milrren Mattar <milrren.mattar@minutrade.com>
2016-10-04 13:25:02 -0300

    Including new test for subtraction operator

--- test/page-objects/main.js
+++ test/page-objects/main.js
@@ -2,17 +2,24 @@
 
 var inputFirst = element(by.model('first'));
 var inputSecond = element(by.model('second'));
+var selectOperator = element(by.model('operator'));
 var buttonGo = element(by.id('gobutton'));
 var textLatest = element(by.binding('latest'));
 
 return module.exports = {
   elements: {
-    latest: textLatest
+    latestResult: textLatest
   },
   flows: {
     sum: function(first, second) {
       inputFirst.sendKeys(first);
       inputSecond.sendKeys(second);
+      buttonGo.click();
+    },
+    subtract: function(first, second) {
+      inputFirst.sendKeys(first);
+      inputSecond.sendKeys(second);
+      selectOperator.sendKeys('-');
       buttonGo.click();
     }
   }
--- test/todo-spec.js
+++ test/todo-spec.js
@@ -6,14 +6,19 @@ describe('Protractor demo app', function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
   });
 
-  it('should add one and two', function() {
+  it('should sum one and two', function() {
     main.flows.sum('1', '2');
-    expect(main.elements.latest.getText()).toEqual('3');
+    expect(main.elements.latestResult.getText()).toEqual('3');
   });
 
-  it('should add three and seven', function() {
+  it('should sum three and seven', function() {
     main.flows.sum('3', '7');
-    expect(main.elements.latest.getText()).toEqual('10');
+    expect(main.elements.latestResult.getText()).toEqual('10');
+  });
+
+  it('should subtract seven from ten', function() {
+    main.flows.subtract('10', '7');
+    expect(main.elements.latestResult.getText()).toEqual('3');
   });
 
 });
