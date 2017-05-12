0124fb4e2bb13360e544f2ff3546766e4ec836db
Milrren Mattar <milrren.mattar@minutrade.com>
2017-05-12 16:54:16 -0300

    Update .gitignore and add html report to jasmine

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,21 @@
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+
+var timestamp = new Date().toISOString().replace(/\..+/, '');
+console.log(timestamp);
+
+var reporter = new Jasmine2HtmlReporter({
+  consolidateAll: true,
+  savePath: 'reports',
+  takeScreenshotsOnlyOnFailures: true,
+  filePrefix: `report-${timestamp}`
+});
+
 exports.config = {
   framework: 'jasmine2',
-  specs: ['test/todo-spec.js'],
+  onPrepare: () => {
+    jasmine.getEnv().addReporter(reporter);
+  },
+  specs: ['test/**/*.js'],
   directConnect: true,
   multiCapabilities: [
     { browserName: 'chrome' }
